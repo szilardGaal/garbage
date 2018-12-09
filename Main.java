03172c79d183a926147765c005ce70651880af69
szilardGaal <gaalszilard23@gmail.com>
2018-12-10 00:01:23 +0100

    valami

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
 public class Main {
 
     public static void main(String[] args) throws DustbinContentException {
-        Dustbin db = new Dustbin();
-        System.out.println(db);
-    }
+
+        Garbage[] rottenTomatoes = new Garbage[3];
+        for (int i = 0; i < 3; i++) {
+            rottenTomatoes[i] = new Garbage("rotten tomato nr." + (i+1));
+        }
+
+        Garbage milkJug = new PlasticGarbage("plastic milk jug", false);
+        Dustbin dustbin = new Dustbin("Jenny's handsome");
+        dustbin.displayContents();
+
+        for (int i = 0; i < 3; i++) {
+            dustbin.throwOutGarbage(rottenTomatoes[i]);
+        }
+        if (!milkJug.isClean()) {
+            milkJug.clean();
+        }
+        
+        dustbin.throwOutGarbage(milkJug);
+        dustbin.emptyContents();
+        dustbin.displayContents();
+    }   
 }
